c2ff070039be1b7a565bdebca99fa1f09982524c
Thiago Santos <tos.oliveira@gmail.com>
2021-04-18 14:24:28 -0300

    Fixing code smell

--- src/apply-eventify.ts
+++ src/apply-eventify.ts
@@ -2,15 +2,26 @@ import { Class, Func, isClass } from 'is-this-a-pigeon';
 import { Eventify } from './eventify-decorator';
 import { EventifyApplier } from './types';
 
+function getApplier(
+	ref: unknown,
+	getInstance: <T extends EventifyApplier<Func>>(cls: Class<T>) => T,
+) {
+	return isClass(ref) ? getInstance(ref) : undefined;
+}
+
+function validateGetInstance(value: unknown) {
+	if (!value) {
+		throw new TypeError('getInstance must be specified');
+	}
+}
+
 export function applyEventify(
 	getInstance?: <T extends EventifyApplier<Func>>(cls: Class<T>) => T,
 ) {
-	if (!getInstance) {
-		throw new TypeError('getInstance must be specified');
-	}
+	validateGetInstance(getInstance);
 	for (const item of Eventify) {
 		const ref = item.args[0];
-		const applier = isClass(ref) ? getInstance(ref) : undefined;
+		const applier = getApplier(ref, getInstance!);
 		if (applier) {
 			applier.applyListeners(item.target[item.name as keyof Object] as any);
 		}
