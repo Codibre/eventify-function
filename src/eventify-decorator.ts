53b8d4bf7c625a006047cc3eaf2730b549006c81
Thiago Santos <tos.oliveira@gmail.com>
2021-05-13 17:25:39 -0300

    fixing fixed applyEventify

--- src/eventify-decorator.ts
+++ src/eventify-decorator.ts
@@ -2,20 +2,21 @@ import { createMethodDecorator } from 'decorator-builder';
 import { AbstractClass, Func, isClass } from 'is-this-a-pigeon';
 import { eventifiedList } from './eventified-list';
 import { eventifyFunction } from './eventify-function';
-import { EventifyApplier } from './types';
+import { EventifiedFunc, EventifyApplier } from './types';
 
 export const Eventify = createMethodDecorator<
 	(
 		applierClass: AbstractClass<EventifyApplier<Func>> | EventifyApplier<Func>,
 	) => void
 >((item) => {
-	const eventified = eventifyFunction(
+	const eventified: EventifiedFunc<Func> = eventifyFunction(
 		item.target[item.name as keyof Object] as any,
 	);
 	item.target[item.name as keyof Object] = eventified;
-	eventifiedList.push(eventified);
 	const applier = item.args[0];
-	if (!isClass(applier)) {
+	if (isClass(applier)) {
+		eventifiedList.push([eventified, applier]);
+	} else {
 		(applier as EventifyApplier<Func>).applyListeners(eventified);
 	}
 });
